60d39e2e1aaf3f049fda03fd0112974c737161ee
Gustavo Pilla <gustavopillacoutinhogmail.com>
2021-03-02 21:12:31 -0300

    removed old dist

--- dist/Resource/Routes/ExampleResource.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prefix = '/api';
-const CallCostRouteResource = {
-    post: `${prefix}`,
-    get: `${prefix}`
-};
-exports.default = CallCostRouteResource;
-//# sourceMappingURL=ExampleResource.js.map
